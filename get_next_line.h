c7a2e3dac5a18967b12d27d71810094b46850fe3
beaujard mael <beaujardmael@mbp-de-beaujard.home>
2020-08-11 21:38:01 +0200

    gnl v1 // norminette + test + bonus

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbeaujar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/11 15:33:20 by mbeaujar          #+#    #+#             */
+/*   Updated: 2020/08/11 21:27:32 by mbeaujar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# define MAX_FD 2147483647
+
+char		*ret_line(char *save);
+char		*next_line_buff(char *save);
+int			get_next_line(int fd, char **line);
+size_t		ft_strlen(const char *s);
+int			is_endl(char *save);
+char		*ft_strjoin(char *s1, char *s2);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+
+#endif
